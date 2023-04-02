757c3f6892cb1c7b44b80d5c4a85ca2df1373927
Mariano Ezequiel Paporello <98173967+mariano-paporello@users.noreply.github.com>
2023-04-01 22:38:44 -0300

    delete dist folder

--- dist/src/models/messages/messages.js
+++
@@ -1,4 +0,0 @@
-// import mongoose from "mongoose"
-// const menssages= 'menssages'
-// const menssagesModel = mongoose.model(menssages, messagesSchema)
-// export default menssagesModel
--- dist/src/routes/profile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var jws_1 = require("../api/jws");
-var auth_1 = require("../controller/auth");
-var profile_1 = require("../controller/profile");
-var ProfileRoute = (0, express_1.Router)();
-ProfileRoute.get("/", jws_1.checkAuth, auth_1.isLogged, auth_1.loggedIsNotDestroyed, profile_1.profileGet);
-exports.default = ProfileRoute;
